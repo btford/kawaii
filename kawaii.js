14d3e13c0caef0f5910c0a1ff1e60056e45d17d5
Po-Ying Chen <poying.me@gmail.com>
2014-05-26 20:54:02 +0800

    Support both CommonJS and AMD

--- kawaii.js
+++ kawaii.js
@@ -1,19 +1,29 @@
-var thingsThatAreKawaii = [
-  /^baby/,
-  /kitten/,
-  /brian ford/
-];
+;(function (name, definition) {
+  if (typeof module !== 'undefined') {
+    module.exports = definition();
+  } else if (typeof define === 'function' && typeof define.amd === 'object') {
+    define(definition);
+  } else {
+    window[name] = definition();
+  }
+})('kawaii', function () {
+  var thingsThatAreKawaii = [
+    /^baby/,
+    /kitten/,
+    /brian ford/
+  ];
 
-var thingsThatAreNotKawaii = [
-  /spider/
-];
+  var thingsThatAreNotKawaii = [
+    /spider/
+  ];
 
-module.exports = function (thing) {
-  thing = thing.trim().toLowerCase();
+  return function (thing) {
+    thing = thing.trim().toLowerCase();
 
-  return thingsThatAreKawaii.some(function (re) {
-    return re.test(thing);
-  }) && !thingsThatAreNotKawaii.some(function (re) {
-    return re.test(thing);
-  });
-};
+    return thingsThatAreKawaii.some(function (re) {
+      return re.test(thing);
+    }) && !thingsThatAreNotKawaii.some(function (re) {
+      return re.test(thing);
+    });
+  };
+});
